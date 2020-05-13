cad168ab5528cd9dc5ea2361c7ba6b2ba39a83ca
sethrpaniagua <64714324+sethrpaniagua@users.noreply.github.com>
2020-05-13 10:26:12 -0500

    take out personal details

--- SethPaniaguaProgram3A.py
+++ SethPaniaguaProgram3A.py
@@ -1,23 +1,4 @@
-#---------------------------------
-#Programmer: Seth Paniagua
-#Date Due: Feb. 18, 2020
-#Date of First Version: Feb. 15,2020
-#Date turned in: Feb. 16, 2020
-#Title of Project: Write miles driven and gased used in CSV 
-#Assignment: Assignment 3A
-#----------------------------------
-#Design
-#Open File for Writing
-#Create variable to continue the program using 'Y'
-#Create While Loop to continue as long as user inputs 'Y'
-#User Input Miles Driven
-#User Input Gas Used
-#write miles driven into CSV file and go to new line using \n
-#write gas used into CSV file and go to new line using \n
-#ask user if they would like to continue and promt them the command to continue
-#close file and print "Finished writing file"
-#print statement "Program completed saving X mileage records in CSV file
-#------------------------------------
+
 
 #Open File
 mpg_file = open('E:\mileage.csv', 'w')
